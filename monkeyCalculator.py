7b6ffdf759acfb70cb931c11b6277ec56059ed6a
Taylor Moog <Taylor.Moog@vu.com>
2023-02-19 18:43:28 -0600

    used if statements

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -3,6 +3,14 @@ def calculateTime():
     # This first line is provided for you
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
+    if monkey_one.upper() == "Y" and monkey_two.upper() == "Y":
+        print("Uh Oh! We're in trouble!")
+    elif monkey_one.upper() == "Y" and monkey_two.upper() == "N":
+        print("Yay! We're going to have a good day!")
+    elif monkey_one.upper() == "N" and monkey_two.upper() == "Y":
+        print("Yay! We're going to have a good day!")
+    elif monkey_one.upper() == "N" and monkey_two.upper() == "N":
+        print("Uh Oh! We're in trouble!")
 
     # end assignment
 
